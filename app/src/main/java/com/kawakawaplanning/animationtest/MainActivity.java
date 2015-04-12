1fbaf2a2943c5cc080bf4bbf789de79c9e877381
kawakawaritsuki.dev@gmail.com <kawakawakun>
2015-04-12 20:52:52 +0900

    New

--- app/src/main/java/com/kawakawaplanning/animationtest/MainActivity.java
+++ app/src/main/java/com/kawakawaplanning/animationtest/MainActivity.java
@@ -1,3 +1,10 @@
+/*
+/
+/参考サイト
+/http://techbooster.jpn.org/andriod/application/1680/
+/
+ */
+
 package com.kawakawaplanning.animationtest;
 
 import android.content.Intent;
@@ -6,21 +13,30 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.RotateAnimation;
+import android.view.animation.ScaleAnimation;
+import android.view.animation.TranslateAnimation;
 import android.widget.ImageView;
 
 
 public class MainActivity extends ActionBarActivity {
 
     private AnimationDrawable anim;
-    ImageView image;
+    private ImageView image;
+    private ImageView animImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        image = (ImageView) findViewById(R.id.image_animation);
-        image.setBackgroundResource( R.drawable.frame );
+
+        image = (ImageView)findViewById(R.id.image_animation);
+        image.setBackgroundResource(R.drawable.frame );
         anim = (AnimationDrawable)image.getBackground();
+
+        animImage = (ImageView)findViewById(R.id.imageView);
+
     }
 
     public void intent(View v){
@@ -35,5 +51,35 @@ public class MainActivity extends ActionBarActivity {
 
     public void Stop(View v){
         anim.stop();
+    }
+
+    public void Alpha(View v) {
+        AlphaAnimation alpha = new AlphaAnimation(1, 0.1f);//1から0.1に
+        alpha.setDuration(1000);//アニメーション時間(ms単位)
+        animImage.startAnimation(alpha);
+    }
+
+    public void Rotate(View v) {
+        RotateAnimation rotate = new RotateAnimation(0, 360, animImage.getWidth()/2, animImage.getHeight()/2);//中心を軸xyで指定して、360度回転
+        rotate.setDuration(1000);//アニメーション時間(ms単位)
+        animImage.startAnimation(rotate);
+    }
+
+    public void ScaleUp(View v) {
+        ScaleAnimation scale = new ScaleAnimation(1, 2, 1, 2);//２倍に拡大
+        scale.setDuration(1000);//アニメーション時間(ms単位)
+        animImage.startAnimation(scale);
+    }
+
+    public void ScaleDown(View v) {
+        ScaleAnimation scale = new ScaleAnimation(1, 0.5f, 1, 0.5f);//半分に縮小
+        scale.setDuration(1000);//アニメーション時間(ms単位)
+        animImage.startAnimation(scale);
+    }
+
+    public void Translate(View v) {
+        TranslateAnimation translate = new TranslateAnimation(0, 100, 0, 100);//0を起点として100先へ移動
+        translate.setDuration(1000);//アニメーション時間(ms単位)
+        animImage.startAnimation(translate);
     }
 }
